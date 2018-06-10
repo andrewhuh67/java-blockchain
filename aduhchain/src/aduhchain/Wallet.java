490b2dc7ffc795e3dcdd244845653a5887ff2d3d
Andrew <andrewhuh67@gmail.com>
2018-06-09 23:15:39 -0700

    finished mining and working on wallet

---
+++ aduhchain/src/aduhchain/Wallet.java
@@ -0,0 +1,15 @@
+package aduhchain;
+import java.security.*;
+
+public class Wallet {
+	public PrivateKey privateKey;
+	public PublicKey publicKey;
+	
+	public void generateKeyPair() {
+		try {
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC")
+					
+		}
+	}
+}
+
