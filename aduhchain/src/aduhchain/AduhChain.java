d77180d4aedc80a9daf436b07a9f6d35752aa74d
Andrew <andrewhuh67@gmail.com>
2018-06-09 22:13:33 -0700

    started creating blockchain in java

---
+++ aduhchain/src/aduhchain/AduhChain.java
@@ -0,0 +1,16 @@
+package aduhchain;
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+
+public class AduhChain {
+	public static void main(String[] args) {
+		Block genesisBlock = new Block("Hi im the first Block", "0");
+		System.out.println("Hash for block 1 : " + genesisBlock.hash);
+		
+		Block secondBlock = new Block("Yo im the second", genesisBlock.hash);
+		System.out.println("Hash for block 2 : " + secondBlock.hash);
+		
+		Block thirdBlock = new Block("Hey im the third block", secondBlock.hash);
+		System.out.println("Hash for block 3 : " + thirdBlock.hash);
+	}
+}
