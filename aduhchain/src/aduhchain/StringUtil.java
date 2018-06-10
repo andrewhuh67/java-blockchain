2e01811e03cb3cf57b897c51686d87101d7e977e
Andrew <andrewhuh67@gmail.com>
2018-06-10 10:42:48 -0700

    working on transaction and wallet

--- aduhchain/src/aduhchain/StringUtil.java
+++ aduhchain/src/aduhchain/StringUtil.java
@@ -1,5 +1,7 @@
 package aduhchain;
 import java.security.MessageDigest;
+import java.security.Signature;
+import java.util.Base64;
 
 public class StringUtil {
 	public static String applySha256(String input) {
@@ -21,5 +23,34 @@ public class StringUtil {
 		catch(Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+	public static byte[] applyECDSASig(PrivateKey privateKey, String input) {
+		Signature dsa;
+		byte[] output = new byte[0];
+		try {
+			dsa = Signature.getInstance("ECDSA", "BC");
+			dsa.initSign(privateKey);
+			byte[] strByte = input.getBytes();
+			dsa.update(strByte);
+			byte[] realSig = dsa.sign();
+			output = realSig;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return output;
+		
+	}
+	public static boolean verifyECDSASign(PublicKey publicKey, String data, byte[] signature) {
+		try {
+			Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+			ecdsaVerify.initVerify(publicKey);
+			ecdsaVerify.update(data.getBytes());
+			return ecdsaVerify.verify(signature);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	public static String getStringFromKey(Key key) {
+		return Base64.getEncoder().encodeToString(key.getEncoded());
 	}
 }
--- aduhchain/src/aduhchain/Wallet.java
+++ aduhchain/src/aduhchain/Wallet.java
@@ -5,10 +5,21 @@ public class Wallet {
 	public PrivateKey privateKey;
 	public PublicKey publicKey;
 	
+	public Wallet() {
+		generateKeyPair();
+	}
+	
 	public void generateKeyPair() {
 		try {
-			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC")
-					
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
+			SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+			ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+			keyGen.initilize(ecSpec, random);
+			KeyPair keyPair = keyGen.generateKeyPair();
+			privateKey = keyPair.getPrivate();
+			publicKey = keyPair.getPublic();
+		}catch(Exception e) {
+			throw new RuntimeException(e);
 		}
 	}
 }
